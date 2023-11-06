475e834c4b25303598862ff69f6b9ee188892ed3
NirSheva <sekoudabo884@gmail.com>
2023-11-06 22:12:39 +0100

    add all folders and files

---
+++ JS/main.js
@@ -0,0 +1,142 @@
+//localStorage.clear();
+
+// Définition de la variable template
+const template = document.getElementById('recommendation-template');
+
+// Asynchronous function to fetch recommendations from local storage or a JSON file
+async function fetchRecommendations() {
+  let allRecommendations = window.localStorage.getItem('recommendations');
+  // Set allRecommendations to null to force data fetch
+  // allRecommendations = null;
+  if (allRecommendations === null) {
+    console.log("Fetching recommendations...");
+    try {
+      const response = await fetch("./DATABASE/recommendations.json");
+      allRecommendations = await response.json();
+
+      // Save fetched data to local storage
+      window.localStorage.setItem("recommendations", JSON.stringify(allRecommendations));
+      return allRecommendations;
+    } catch (error) {
+      console.error("Error fetching data from JSON file:", error);
+    }
+  } else {
+    console.log("Data found in local storage.");
+    allRecommendations = JSON.parse(allRecommendations);
+    return allRecommendations;
+  }
+}
+
+// Function to display a popup for adding a new recommendation
+function showPopup(bool) {
+  const popup = document.getElementById('popup');
+  popup.style.visibility = bool ? "visible" : "hidden";
+}
+
+// Reload the page to display the last recommendation
+const okButton = document.querySelector('.popup button');
+okButton.addEventListener("click", function () {
+  // Reload page
+  location.reload();
+});
+
+// Asynchronous function to display recommendations
+async function displayRecommendations() {
+  try {
+    const recommendations = await fetchRecommendations();
+
+    if (recommendations && recommendations.length > 0) {
+      const recommendationsContainer = document.querySelector('.carousel-inner.all-recommendations-container');
+
+      let first = true;
+
+      for (let recommendation of recommendations) {
+        const clone = document.importNode(template.content, true);
+        const cardTitle = clone.querySelector('.card-title');
+        const cardText = clone.querySelector('.card-text');
+        const dateContainer = clone.querySelector('.card-date');
+
+        cardText.textContent = `“ ${recommendation["content"]} ”` || "";
+        cardTitle.textContent = recommendation["author"] || "Anonymous";
+
+        if (recommendation["datetime"] != null) {
+          dateContainer.textContent = recommendation["datetime"];
+        } else {
+          dateContainer.textContent = "-";
+        }
+
+        if (first) {
+          clone.querySelector('.carousel-item').classList.add('active');
+          first = false;
+        }
+        recommendationsContainer.appendChild(clone);
+      }
+
+    } else {
+        const clone = document.importNode(template.content, true);
+        const cardTitle = clone.querySelector('.card-title');
+        const cardText = clone.querySelector('.card-text');
+        const dateContainer = clone.querySelector('.card-date');
+
+        cardText.textContent = " No recommendation ";
+        cardTitle.textContent = "";
+        dateContainer.textContent = "-";
+    }
+  } catch (error) {
+    console.error("An error occurred while displaying recommendations:", error);
+  }
+}
+
+// Asynchronous function to add a new recommendation
+async function addRecommendation() {
+  // Get the current date
+  const currentDate = new Date();
+  const dateString = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+
+  // Get the owner and message of the new recommendation
+  const valInput = document.getElementById('title-recommendation').value;
+  const valTextarea = document.getElementById('new-recommendation').value;
+
+  // If the user has left a recommendation, display a pop-up
+  if (valTextarea != null && valTextarea.trim() !== "") {
+    console.log("New recommendation added");
+    // Call the showPopup function here
+    showPopup(true);
+
+    const recommendation = {
+      "author": valInput,
+      "content": valTextarea,
+      "datetime": dateString
+    };
+
+    try {
+      // Add the new recommendation to the beginning of the array
+      let allRecommendations = window.localStorage.getItem('recommendations');
+      allRecommendations = JSON.parse(allRecommendations);
+      allRecommendations.unshift(recommendation);
+
+      localStorage.clear();
+      // Save updated recommendations to local storage
+      window.localStorage.setItem("recommendations", JSON.stringify(allRecommendations));
+    } catch (error) {
+      console.error("Error while writing data:", error);
+    }
+  }
+}
+
+// Select the button to scroll to the top
+const scrollToTopButton = document.getElementById("scrollToTopButton");
+
+// Add an event handler for clicking the button
+scrollToTopButton.addEventListener("click", function(e) {
+    e.preventDefault(); // Prevent the link from triggering a page reload
+
+    // Scroll the page to the top
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Smooth scrolling for a fluid transition
+    });
+});
+
+// Main function: Display recommendations
+displayRecommendations();
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# my-portfolio
+# My portfolio
+
+
+![potfolio](./IMAGES/banner-readme.jpeg)
+
+## Description
+
+This project was a personal challenge, and it involved creating my portfolio website from scratch without using templates, using HTML, CSS, JavaScript, and their respective frameworks. The goal was to assess my skills with these technologies.
+
+## License
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+
+## Notes and Suggestions
+
+If you have any comments, suggestions, or questions, feel free to share them by opening a new issue. We appreciate community feedback and are open to continuous improvement of this project.
+
+Thank you for your participation and your interest in this project!
+
+
+## Contacts
+
+For any questions or comments, please feel free to contact me at the following email address: sekoudabo884@gmail.com or by visiting my GitHub profile.
